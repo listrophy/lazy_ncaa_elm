a658d87fb2b18c6016fa5fe44d2787008fefc38f
Brad Grzesiak <brad@bendyworks.com>
2017-03-01 11:41:20 -0600

    Update watch.sh for after-build

--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-elm-live src/Main.elm --output=elm.js --before-build=./compile-css.sh
+elm-live src/Main.elm --output=elm.js --before-build=./compile-css.sh --after-build=./after-build.sh
 # something like `elm-live Main.elm --output=../public/js/app.js --dir=../public  --open —pushstate`
